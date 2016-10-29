1dfb048e178807006ace903b86ba3e2cef9f116e
avitkus <avitkus7@gmail.com>
2016-10-29 14:44:25 -0400

    added PlantSpecies construtor

--- src/ecologysim/PlantSpecies.java
+++ src/ecologysim/PlantSpecies.java
@@ -5,9 +5,16 @@ public class PlantSpecies implements IPlantSpecies {
 	private String name;
 	private String[][] produces;
 	private double[][] growthRates;
-	private double[][] maxProduce;
+	private double[][] maxProduce;	
 	
-	
+	public PlantSpecies(String name, String[][] produces, double[][] growthRates, double[][] maxProduce) {
+		super();
+		this.name = name;
+		this.produces = produces;
+		this.growthRates = growthRates;
+		this.maxProduce = maxProduce;
+	}
+
 	@Override
 	public Kingdom getKingdom() {
 		return Kingdom.PLANT;
