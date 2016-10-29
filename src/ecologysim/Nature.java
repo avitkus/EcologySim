7bcd6ed8a1758161876a41d0d9f4901f9fe6d0e9
Skye Frame <skyeframe@gmail.com>
2016-10-29 17:20:51 -0400

    Reproduce Method

--- src/ecologysim/Nature.java
+++ src/ecologysim/Nature.java
@@ -1,5 +1,8 @@
 package ecologysim;
 
+import java.util.List;
+import java.util.Set;
+
 public class Nature implements INature{
 	
 	private IAnimalDB animals;
@@ -28,7 +31,38 @@ public class Nature implements INature{
 
 	@Override
 	public void reproduce() {
-		// TODO Auto-generated method stub
+		Set<IAnimalSpecies> speciesSet = animals.getMap().keySet();
+		IAnimalSpecies[] speciesList = speciesSet.toArray(new IAnimalSpecies[speciesSet.size()]);
+		for (int i = 0; i < speciesList.length; i++) {
+			List<IAnimal> females = animals.getAllFemales(speciesList[i]);
+			
+			int[] season = speciesList[i].getMatingSeason();
+			if (week%52 >= season[0] && week%52 <= season[1]) {
+				for (int j = 0; j < females.size(); j++) {
+					if (!females.get(j).isPregnant()) {
+						//fix pregnancy rate
+						if (Math.random() > 0.5 && 
+								females.get(j).getAge() < speciesList[i].getOldAge() &&
+								females.get(j).getAge() >= speciesList[i].getAdultAge()) {//make counts
+							females.get(j).setIsPregnant(true);
+							females.get(j).setLastPregnancy(week);
+						} else if (females.get(j).isPregnant() && 
+								females.get(j).getLastPregnancy()+speciesList[i].getGestationPeriod() == week) {
+							//Fix Probabilities
+							animals.addAnimal(speciesList[i], 2, 0, 0, 0);
+						}
+					}
+				}
+			} else {
+				for (int j = 0; j < females.size(); j++) {
+					if (females.get(j).isPregnant() && 
+							females.get(j).getLastPregnancy()+speciesList[i].getGestationPeriod() == week) {
+						//Fix Probabilities
+						animals.addAnimal(speciesList[i], 2, 0, 0, 0);
+					}
+				}
+			}
+		}
 		
 	}
 
