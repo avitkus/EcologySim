028ce8aa75a627b7b2cdb40397ca4dc38ab0a284
avitkus <avitkus7@gmail.com>
2016-10-29 13:37:32 -0400

    added data file reader

---
+++ src/ecologysim/SpeciesFileReader.java
@@ -0,0 +1,15 @@
+package ecologysim;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+
+public class SpeciesFileReader {
+	static Optional<IAnimalSpecies[]> processAnimals(Path file) throws IOException {
+		return Optional.empty();
+	}
+	
+	static Optional<IPlantSpecies[]> processPlants(Path file) throws IOException {
+		return Optional.empty();
+	}
+}
